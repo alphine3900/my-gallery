22e95371ade324e88a70c9181e016986364501d1
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-28 23:11:05 +0300

    added test file

--- app2/tests.py
+++ app2/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
-
+from .models import Category, Image, Location
 # Create your tests here.
+class PhotoTestClass(TestCase):
+    def setUp(self):
+        self.new_photo = Image('new_url')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_photo, Image))
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.save_location= Location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.save_location, Location))
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.save_category= Category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.save_category, Category))
